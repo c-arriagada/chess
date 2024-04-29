3bc3136abfc2f5d1a2cb91eb6d78fc2f3cdd4e1d
Caheri <caheri.aguilar@gmail.com>
2024-04-29 11:21:46 -0500

    added move rules to bishops and rooks, displayed board to be more readable.

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-const prompt=require('prompt-sync')({sigint:true})
+const prompt = require("prompt-sync")({ sigint: true });
 
 // 8x8 board
 // black and white pieces
@@ -13,73 +13,137 @@ const prompt=require('prompt-sync')({sigint:true})
 // Knight - Moves in an ‘L-shape,’ two squares in a straight direction, and then one square perpendicular to that.
 // Pawn - Moves one square forward, but on its first move, it can move two squares forward. It captures diagonally one square forward.
 
-
 const board = [
-                ['WR', 'WK', 'WB', 'WK', 'WQ', 'WB', 'WK', 'WR'],
-                ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
-                ['', '', '', '', '', '', '', ''],
-                ['', '', '', '', '', '', '', ''],
-                ['', '', '', '', '', '', '', ''],
-                ['', '', '', '', '', '', '', ''],
-                ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
-                ['BR', 'BK', 'BB', 'BK', 'BQ', 'BB', 'BK', 'BR']
-            ]
+  ["WR", "WK", "WB", "WK", "WQ", "WB", "WK", "WR"],
+  ["WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+  ["BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"],
+  ["BR", "BK", "BB", "BK", "BQ", "BB", "BK", "BR"],
+];
 
-let rowsCols = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8} // values of columns - y value
+let rowsCols = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }; // values of columns - y value
 
-// ask user for input. letters = columns, numbers = rows
-let playerInputPiece = prompt("Tell me the location of the piece you'd like to move. e.g. 'a1':" )
-let playerInputNewLocation = prompt("Where would you like to move this piece to? e.g. 'a4':" )
+const playerInput = (player) => {
+  // ask user for input. letters = columns, numbers = rows
+  let playerInputPiece = prompt(
+    `${player} Tell me the location of the piece you'd like to move. e.g. 'a1': `
+  );
+  let playerInputNewLocation = prompt(
+    "Where would you like to move this piece to? e.g. 'a4':"
+  );
 
-let currentRow = Number(playerInputPiece.slice(1)) - 1 
-let currentColumn = Number(rowsCols[playerInputPiece.slice(0,1)]) - 1
-let currentPiece = board[currentRow][currentColumn]
-console.log(playerInputPiece, currentPiece)
-// get values for row and column of new location 
-let newRow = Number(playerInputNewLocation.slice(1)) - 1
-let newColumn = Number(rowsCols[playerInputNewLocation.slice(0,1)]) - 1
-let pieceAtNewLocation = board[newRow][newColumn]
-console.log('Current piece at new location', pieceAtNewLocation)
-// reassign new location to piece chosen by player
-board[newRow][newColumn] = currentPiece
-// reassign the value of an empty string to the location where you are moving the piece from
-board[currentRow][currentColumn] = ''
-console.log('new location', playerInputNewLocation)
-console.log(`piece at new location row at index${newRow} column at index ${newColumn} is ${board[newRow][newColumn]}`)
-console.log(board)
+  return [playerInputPiece, playerInputNewLocation];
+};
 
-const pawnMoves = (currentPos, newPos) => {
-    let [xCurr, yCurr] = currentPos;
-    let [xNew, yNew] = newPos;
+const move = (currentLocation, newLocation) => {
+  // get row and column of current location
+  let currentRow = Number(currentLocation.slice(1)) - 1;
+  let currentColumn = Number(rowsCols[currentLocation.slice(0, 1)]) - 1;
+  let currentPiece = board[currentRow][currentColumn];
 
-    if(yNew === yCurr + 1 || yCurr - 1) {
-        console.log('y movement is valid', yNew)
+  // check if piece is a pawn
+  if (currentPiece.slice(1) === "P") {
+    console.log(movePawn(currentLocation, newLocation));
+    // check if piece is a rook
+  } else if (currentPiece.slice(1) === "R") {
+    console.log(moveRook(currentLocation, newLocation))
+    // check if piece is a bishop
+  } else if (currentPiece.slice(1) === "B") {
+    console.log(moveBishop(currentLocation, newLocation))
+  }
+
+  console.log(
+    `Piece ${currentPiece} moved from ${currentLocation} to ${newLocation}`
+  );
+};
+
+const movePawn = (currentLocation, newLocation) => {
+  // get row and column of current location
+  let currentRow = Number(currentLocation.slice(1)) - 1;
+  let currentColumn = Number(rowsCols[currentLocation.slice(0, 1)]) - 1;
+  let currentPiece = board[currentRow][currentColumn];
+  // get row and column of new location
+  let newRow = Number(newLocation.slice(1)) - 1;
+  let newColumn = Number(rowsCols[newLocation.slice(0, 1)]) - 1;
+
+  if (
+    currentRow === newRow &&
+    (newColumn === currentColumn + 1 || currentColumn - 1)
+  ) {
+    board[newRow][newColumn] = currentPiece;
+    board[currentRow][currentColumn] = "  ";
+    return "Pawn was moved one space to the left or right.";
+  } else if (
+    currentColumn === newColumn &&
+    (newRow === currentRow + 1 || currentRow - 1)
+  ) {
+    board[newRow][newColumn] = currentPiece;
+    board[currentRow][currentColumn] = "  ";
+    return "Pawn was moved one space forward or backward";
+  } else {
+    return "Invalid move";
+  }
+};
+
+const moveRook = (currentLocation, newLocation) => {
+  // get row and column of current location
+  let currentRow = Number(currentLocation.slice(1)) - 1;
+  let currentColumn = Number(rowsCols[currentLocation.slice(0, 1)]) - 1;
+  let currentPiece = board[currentRow][currentColumn];
+  // get row and column of new location
+  let newRow = Number(newLocation.slice(1)) - 1;
+  let newColumn = Number(rowsCols[newLocation.slice(0, 1)]) - 1;
+
+  if(currentRow === newRow && (0 <= newColumn <= 7)) {
+    board[newRow][newColumn] = currentPiece;
+    board[currentRow][currentColumn] = "  ";
+    return "Rook moved horizontally"
+  } else if(currentColumn === newColumn && (0 <= newRow <= 7)) {
+    board[newRow][newColumn] = currentPiece;
+    board[currentRow][currentColumn] = "  ";
+    return "Rook moved vertically"
+  } else {
+    return "Invalid move"
+  }
+};
+
+const moveBishop = (currentLocation, newLocation) => {
+    // get row and column of current location
+    let currentRow = Number(currentLocation.slice(1)) - 1;
+    let currentColumn = Number(rowsCols[currentLocation.slice(0, 1)]) - 1;
+    let currentPiece = board[currentRow][currentColumn];
+    // get row and column of new location
+    let newRow = Number(newLocation.slice(1)) - 1;
+    let newColumn = Number(rowsCols[newLocation.slice(0, 1)]) - 1;
+    // checking for diagonal move
+    // TODO: find a better way to check for diagonal movement
+    if(newRow !== currentRow && (newColumn !== currentColumn)) {
+      board[newRow][newColumn] = currentPiece;
+      board[currentRow][currentColumn] = "  ";
+      return "Bishop moved diagonally"
     } else {
-        return 'y movement is invalid, pawn can only move 1 up or down'
+      return "Invalid move"
     }
-    if(xNew === xCurr + 1 || xCurr - 1) {
-        console.log('x movement is valid', xNew)
-    } else {
-        return 'x movement is invalid, pawn can only move 1 left or right'
-    }
+  };
 
-    // move pawn to new position
-    board[xNew][yNew] = board[xCurr][yCurr]
-    // replace previous space occupied by a pawn with an empty string
-    board[xCurr][yCurr] = ''  
-}
+// TODO: Check if there's a piece in the space you want to move to, if there's a piece from the opposite team and the move is valid you take that piece
 
-// pawns only capture diagonally
-const pawnCaptures = (currentPos, newPos) => {
-    let [xCurr, yCurr] = currentPos;
-    let [xNew, yNew] = newPos
+const playGame = () => {
+  let currentPlayer = "W"; // Player with white pieces starts
+  let rounds = 1;
+  let winner;
 
-    // forward movement, checking for new position being diagonal from starting pos
-    if(xNew === xCurr + 1 && yNew === yCurr-1 ||
-        xNew === xCurr + 1 && yNew === yCurr+1 ) {
-            board[xNew][yNew] = board[xCurr][yCurr]
-            board[xCurr][yCurr] = ''
-        }
-}
+  while (rounds < 2) {
+    let [currentLocationP1, newLocationP1] = playerInput("Player 1");
+    move(currentLocationP1, newLocationP1);
+    let [currentLocationP2, newLocationP2] = playerInput("Player 2");
+    move(currentLocationP2, newLocationP2);
+    console.log(board.map((row) => row.join(", ")));
+    rounds += 1;
+  }
+};
 
-
+playGame();
